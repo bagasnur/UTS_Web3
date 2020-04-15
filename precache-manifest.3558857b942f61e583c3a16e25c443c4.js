a576c4559f36f2ac86583c82496faaa371ef0985
bagasnur <bagasnur00@mail.ugm.ac.id>
2020-04-15 15:28:38 +0700

    Updates

---
+++ precache-manifest.3558857b942f61e583c3a16e25c443c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f2efae322735b372feec4f759c28d3",
+    "url": "/UTS_Web3/index.html"
+  },
+  {
+    "revision": "364ec42f059b8a6e845b",
+    "url": "/UTS_Web3/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "b3229f49e04271292528",
+    "url": "/UTS_Web3/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "364ec42f059b8a6e845b",
+    "url": "/UTS_Web3/static/js/2.d0ea47ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/UTS_Web3/static/js/2.d0ea47ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3229f49e04271292528",
+    "url": "/UTS_Web3/static/js/main.05528250.chunk.js"
+  },
+  {
+    "revision": "ec3c87d22626e26243f8",
+    "url": "/UTS_Web3/static/js/runtime-main.c7fe5b13.js"
+  }
+]);
